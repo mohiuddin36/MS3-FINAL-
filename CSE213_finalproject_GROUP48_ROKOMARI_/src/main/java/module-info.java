67cbd4c680972b8e47c44f2fdb42b35b3bf4fd80
mohiuddin36 <islammohiuddin270@gmail.com>
2025-12-05 19:30:58 +0600

    Initial commit - MS3 final project submission

---
+++ CSE213_finalproject_GROUP48_ROKOMARI_/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.cse213_finalproject_group48_rokomari_ {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.cse213_finalproject_group48_rokomari_ to javafx.fxml;
+    exports com.example.cse213_finalproject_group48_rokomari_;
+}
